13cbc6c94181565f0b7698ff627b901887b1cbc6
Raina <xiaofan.xxf@antgroup.com>
2025-11-18 23:00:56 +0800

    Test

--- src/dragonball/src/vm/x86_64.rs
+++ src/dragonball/src/vm/x86_64.rs
@@ -657,7 +657,7 @@ mod tests {
     #[test]
     #[cfg(feature = "tdx")]
     fn test_tdx_init() {
-        let kernel_path = "/tmp/test_resources/kata-containers/vmlinux.container";
+        let kernel_path = "/tmp/test_resources/hello-vmlinux.bin";
         let tdshim_path = "/tmp/test_resources/final.bin";
 
         let boot_args = "console=ttyS0 console=ttyS1 earlyprintk=ttyS1 tty0 reboot=k debug panic=1 pci=off root=/dev/vda1";
