a0851a9d0b7f037b0aff533f4f6658061bf99b02
Raina <xiaofan.xxf@antgroup.com>
2025-11-18 20:08:33 +0800

    Test

--- src/dragonball/src/vm/x86_64.rs
+++ src/dragonball/src/vm/x86_64.rs
@@ -516,7 +516,7 @@ impl Vm {
         vm_memory: &GuestMemoryImpl,
     ) -> std::result::Result<PayloadInfo, StartMicroVmError> {
         let kernel_loader_result =
-            self.load_kernel(vm_memory, None)?;
+            self.load_kernel(vm_memory, Some(GuestAddress(payload_offset)))?;
 
         if kernel_loader_result.kernel_end > (payload_offset + payload_size) {
             Err(StartMicroVmError::TdDataLoader(
