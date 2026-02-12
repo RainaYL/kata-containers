df41e9d997277e84bae0574130778d119360dc3a
Raina <xiaofan.xxf@antgroup.com>
2026-02-12 10:44:20 +0800

    Test

--- src/dragonball/src/vcpu/vcpu_impl.rs
+++ src/dragonball/src/vcpu/vcpu_impl.rs
@@ -473,12 +473,12 @@ impl Vcpu {
                     VcpuExit::MmioRead(addr, data) => {
                         let _ = self.io_mgr.mmio_read(addr, data);
                         self.metrics.exit_mmio_read.inc();
+                        loop {}
                         Ok(VcpuEmulation::Handled)
                     }
                     VcpuExit::MmioWrite(addr, data) => {
                         let _ = self.io_mgr.mmio_write(addr, data);
                         self.metrics.exit_mmio_write.inc();
-                        loop {}
                         Ok(VcpuEmulation::Handled)
                     }
                     VcpuExit::Hlt => {
