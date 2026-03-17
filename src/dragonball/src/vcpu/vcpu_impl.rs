a4d43cd152f5f9b378ea29e213b5ed62edb7a6d1
RayX <rayx@U-C9WDW2V3-2340.local>
2026-03-17 10:41:32 +0800

    Test

--- src/dragonball/src/vcpu/vcpu_impl.rs
+++ src/dragonball/src/vcpu/vcpu_impl.rs
@@ -530,7 +530,9 @@ impl Vcpu {
                                     let idx = (offset >> 1) as usize;
                                     if (offset & 1) == 0 {
                                         registers.redir_table_entries[idx].low = val;
-                                        println!("gsi: {}, vector: {:#x}, masked: {}", idx, registers.redir_table_entries[idx].get_vector(), registers.redir_table_entries[idx].is_masked());
+                                        if (idx == 5) {
+                                            println!("gsi: {}, vector: {:#x}, masked: {}", idx, registers.redir_table_entries[idx].get_vector(), registers.redir_table_entries[idx].is_masked());
+                                        }
                                     } else {
                                         registers.redir_table_entries[idx].high = val;
                                     }
