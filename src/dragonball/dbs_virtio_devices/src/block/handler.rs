b632172bb1725a92e8b71c59e96e2364e734cdd7
RayX <rayx@U-C9WDW2V3-2340.local>
2026-03-18 10:55:48 +0800

    Test

--- src/dragonball/dbs_virtio_devices/src/block/handler.rs
+++ src/dragonball/dbs_virtio_devices/src/block/handler.rs
@@ -351,7 +351,13 @@ impl<AS: DbsGuestAddressSpace, Q: QueueT> InnerBlockEpollHandler<AS, Q> {
             }
         }
 
-        println!("IO complete vcpu_fd: {}, vector: {:#x}", self.vcpu_fd.unwrap(), self.irq.unwrap());
+        let kvm_interrupt = kvm_interrupt {
+            irq: self.irq.unwrap(),
+        };
+        let vcpu_fd = self.vcpu_fd.unwrap();
+
+        let ret = unsafe { ioctl_with_ref(&vcpu_fd, KVM_INTERRUPT(), &kvm_interrupt) };
+        println!("KVM_INTERRUPT ret: {}", ret);
         
         self.queue.notify()
     }
