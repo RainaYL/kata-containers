4f89a890ca87e1034e90404195e92baddbf9a09e
RayX <xxfanraina@gmail.com>
2026-04-10 18:10:08 +0800

    Test

--- src/dragonball/src/vcpu/vcpu_impl.rs
+++ src/dragonball/src/vcpu/vcpu_impl.rs
@@ -453,12 +453,16 @@ impl Vcpu {
                 match run {
                     #[cfg(target_arch = "x86_64")]
                     VcpuExit::IoIn(addr, data) => {
+                        if addr == 0x3f8 {
+                            println!("ttyS0 in");
+                        }
                         let _ = self.io_mgr.pio_read(addr, data);
                         self.metrics.exit_io_in.inc();
                         Ok(VcpuEmulation::Handled)
                     }
                     #[cfg(target_arch = "x86_64")]
                     VcpuExit::IoOut(addr, data) => {
+                        println!("ttyS0 out");
                         let data = data.to_vec();
                         if !self.check_io_port_info(addr, &data)? {
                             let _ = self.io_mgr.pio_write(addr, &data);
