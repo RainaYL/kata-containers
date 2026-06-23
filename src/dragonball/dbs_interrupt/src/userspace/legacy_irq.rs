8e51d81bd5f09a9b54f7b8d9d0263bbf34bcba5a
Xiaofan Xxf <xiaofan.xxf@antgroup.com>
2026-06-23 18:23:59 +0800

    Test

--- src/dragonball/dbs_interrupt/src/userspace/legacy_irq.rs
+++ src/dragonball/dbs_interrupt/src/userspace/legacy_irq.rs
@@ -77,6 +77,9 @@ impl UserspaceLegacyIrqObj {
     }
 
     fn signal_msi(&self) -> Result<()> {
+        if self.base == 2 {
+            println!("Timer interrupt");
+        }
         let mut address_lo = MsiAddressLow::default();
         address_lo.set_dest_mode_logical(self.redir_entry_low().dest_mode_logical());
         address_lo.set_virt_destid_8_14(self.redir_entry_high().virt_destid_8_14());
