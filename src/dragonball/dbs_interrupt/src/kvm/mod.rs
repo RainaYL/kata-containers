6bea497c02b494db4f8de58c22242abded685ae8
Xiaofan Xxf <xiaofan.xxf@antgroup.com>
2026-07-15 16:59:24 +0800

    Test

--- src/dragonball/dbs_interrupt/src/kvm/mod.rs
+++ src/dragonball/dbs_interrupt/src/kvm/mod.rs
@@ -186,7 +186,7 @@ impl KvmIrqRouting {
         #[allow(unused_mut)]
         let mut routes = self.routes.lock().unwrap();
 
-        #[cfg(feature = "kvm-legacy-irq")]
+        #[cfg(all(feature = "kvm-legacy-irq", not(feature = "split-legacy-irq")))]
         LegacyIrq::initialize_legacy(&mut routes)?;
 
         self.set_routing(&routes)?;
